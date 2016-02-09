c86963314a644c41af88d53ac45989e6eb2484db
Raphael Uzan <raph.a.uzan@gmail.com>
2016-02-09 16:17:18 +0100

    Add Direction and Speed control (to be tested)

--- src/Control.cpp
+++ src/Control.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "Control.h"
+#include "MyRioHelper.h"
+#include <string>
 
 Control::Control() {
 	// TODO Auto-generated constructor stub
@@ -16,3 +18,33 @@ Control::~Control() {
 	// TODO Auto-generated destructor stub
 }
 
+SpeedControl::SpeedControl() : pwmUsed(MRio.Pwm.A0)
+{
+	pwmUsed.setFrequency(FREQUENCY_50HZ);
+}
+
+void SpeedControl::setSpeed(float speed, bool forward) {
+	if(speed < 0 || speed > 1)  throw "Speed should be between 0 and 1";
+
+	float duty = 0;
+	if(forward){
+		duty = MINDUTYFORWARD + (MAXDUTYFORWARD - MINDUTYFORWARD) * speed;
+	} else {
+		duty = MAXDUTYBACKWARD - (MAXDUTYBACKWARD - MINDUTYBACKWARD) * speed;
+	}
+	pwmUsed.setDuty(duty);
+}
+
+DirectionControl::DirectionControl() : pwmUsed(MRio.Pwm.A1)
+{
+	pwmUsed.setFrequency(FREQUENCY_50HZ);
+}
+
+void DirectionControl::setAngle(float angle) {
+	if(angle < MINANGLE || angle > MAXANGLE) {
+		throw "Angle should be between -45 and 45";
+	}
+	this->angle = angle;
+	float duty = MINDUTY + (MAXDUTY - MINDUTY) * ((angle - MINANGLE) / (MAXANGLE - MINANGLE));
+	pwmUsed.setDuty(duty);
+}
--- src/Control.h
+++ src/Control.h
@@ -10,10 +10,59 @@
 
 #include "PWMHelper.h"
 
+class SpeedControl{
+private:
+	static constexpr float MINDUTYFORWARD = 0.065f;
+	static constexpr float MAXDUTYFORWARD = 0.085f;
+	static constexpr float MINDUTYBACKWARD = 0.040f;
+	static constexpr float MAXDUTYBACKWARD = 0.055f;
+	Pwm& pwmUsed;
+
+	float _speed = 0;
+	bool _forward = true;
+public:
+	SpeedControl();
+
+	/* set the speed and direction
+	 * @param speed the speed in % (from 0.00 to 1.00)
+	 * @param forward Optional set to false to go backward
+	 */
+	void setSpeed(float speed, bool forward = true);
+
+	/* @return the direction; true = forward */
+	bool getDirection() const { return _forward; }
+	/* @return the current "target" speed (in %) */
+	float getSpeed() const { return _speed; }
+	Pwm& getPwmUsed() const { return pwmUsed;}
+};
+class DirectionControl {
+private:
+	Pwm& pwmUsed;
+	static constexpr float MINDUTY = 0.05f;
+	static constexpr float MAXDUTY = 0.10f;
+
+	float angle = 0;
+public:
+	DirectionControl();
+	static constexpr float MAXANGLE = 45.0f; /* in degree */
+	static constexpr float MINANGLE = -45.0f; /* in degree */
+	/* set the angle from -45 to 45 degree */
+	void setAngle(float angle);
+	/* @return the angle in degree */
+	float getAngle() const { return angle; }
+	/* set the pwm used (by ref)
+	 * ex: control.setPwmUsed(MRio.Pwm.A0)
+	 */
+	void setPwmUsed(Pwm& pwm) { pwmUsed = pwm;}
+	Pwm& getPwmUsed() const { return pwmUsed;}
+};
 class Control {
 public:
 	Control();
 	virtual ~Control();
+
+	SpeedControl Speed;
+	DirectionControl Direction;
 };
 
 #endif /* CONTROL_H_ */
