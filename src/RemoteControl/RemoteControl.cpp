616ce102a033aad0c6b1362f293e959ff9bf8b7a
Raphael Uzan <raph.a.uzan@gmail.com>
2016-03-02 12:43:29 +0100

    just saving

--- src/RemoteControl/RemoteControl.cpp
+++ src/RemoteControl/RemoteControl.cpp
@@ -9,51 +9,65 @@
 #include <cstring>
 #include <vector>
 
-RemoteControl::RemoteControl(MyRioCar& c) : car(c){
+RemoteControl::RemoteControl(MyRioCar& c) :
+		car(c) {
 	// TODO Auto-generated constructor stub
 
 }
 
 void RemoteControl::serverThread() {
-	/* first identify the remote end
-	 * we wait until data is comming on the socket
-	 * then data is only accepted from the client that sent the first data
-	 */
-	size_t slen = sizeof(remote);
+	cout << "Starting server thread" << endl;
 
-	std::vector<char> headerBuff(sizeof(remote_message_header));
-	std::vector<char> buff(0);
+	try {
+		/* first identify the remote end
+		 * we wait until data is comming on the socket
+		 * then data is only accepted from the client that sent the first data
+		 */
+		size_t slen = sizeof(remote);
 
-	message_t message;
-	int n = 0;
-	do {
-		n = recvfrom(_sock, headerBuff.data(), headerBuff.size(), 0,
-				(struct sockaddr*) &remote, &slen);
+		std::vector<char> headerBuff(sizeof(remote_message_header));
+		std::vector<char> buff(0);
 
-		message.header =
-				*(reinterpret_cast<remote_message_header*>(headerBuff.data()));
-	} while (message.header.type != MESSAGE_HELLO && n > 0); // First we wait until a client say HELLO and we store his address in this->remote
+		message_t message;
+		int n = 0;
+		do {
+			cout << "Waiting for message" << endl;
+			n = recvfrom(_sock, headerBuff.data(), headerBuff.size(), 0,
+					(struct sockaddr*) &remote, &slen);
+			if (n > 0) {
+				message.header =
+						*(reinterpret_cast<remote_message_header*>(headerBuff.data()));
+				cout << "Message comming : " << message.header.type << endl;
+			}
+		} while (message.header.type != MESSAGE_HELLO && n > 0); // First we wait until a client say HELLO and we store his address in this->remote
 
-	// then we start looping forever until we got a msg of the server stops
+		if (n > 0)
+			cout << "Hello message arrived" << endl;
+		// then we start looping forever until we got a msg of the server stops
 
-	while ((n = recvfrom(_sock, headerBuff.data(), headerBuff.size(), 0, NULL,
-			NULL)) > 0) {
-		// first receive the header with the type and length of the message
-		message.header =
-				*(reinterpret_cast<remote_message_header*>(headerBuff.data()));
+		while ((n = recvfrom(_sock, headerBuff.data(), headerBuff.size(), 0,
+				NULL,
+				NULL)) > 0) {
+			// first receive the header with the type and length of the message
+			message.header =
+					*(reinterpret_cast<remote_message_header*>(headerBuff.data()));
 
-		buff.reserve(message.header.length);
-		if (recvfrom(_sock, buff.data(), message.header.length, 0, NULL, NULL)
-				> 0) {
-			message.message = buff.data();
-			dispatchMessage(message);
+			cout << " Message coming:" << message.header.type << endl;
+			buff.reserve(message.header.length);
+			if (recvfrom(_sock, buff.data(), message.header.length, 0, NULL,
+					NULL) > 0) {
+				message.message = buff.data();
+				dispatchMessage(message);
 
-		} else {
-			throw "Protocol error";
+			} else {
+				throw "Protocol error";
+			}
 		}
+		if (n == -1)
+			throw "Unexpected end";
+	} catch (const char* e) {
+		cout << e << endl;
 	}
-	if (n == -1)
-		throw "Unexpected end";
 }
 
 RemoteControl::~RemoteControl() {
@@ -72,26 +86,33 @@ void RemoteControl::startServer(uint16_t port) {
 	me.sin_family = AF_INET;
 	me.sin_port = htons(port);
 	me.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (::bind(_sock, (const struct sockaddr*) &me, sizeof(me)) == -1)
+	if (bind(_sock, (const struct sockaddr*) &me, sizeof(me)) == -1)
 		throw "bind()";
 
 	server_thread = new thread(&RemoteControl::serverThread, this);
 }
 
 void RemoteControl::send(const string& message) {
-	if(sendto(_sock, message.c_str(), message.length(), 0, (const sockaddr*)&remote, sizeof(remote)) == -1) throw "Error sendto()";
+	if (sendto(_sock, message.c_str(), message.length(), 0,
+			(const sockaddr*) &remote, sizeof(remote)) == -1)
+		throw "Error sendto()";
 }
 
 void RemoteControl::dispatchMessage(message_t message) {
+	cout << "Dispatching message ";
 	switch (message.header.type) { // reinterpret depending on the type of the message
-	case MESSAGE_CMD:{
+	case MESSAGE_CMD: {
+		cout << "(type: message_cmd)" << endl;
 		remote_command* cmd = reinterpret_cast<remote_command*>(message.message);
+		cout << "CMD Message: speed: " << cmd->speed << ", angle: "
+				<< cmd->angle << endl;
 		// set the speed and angle of the car
 		car.Control.Speed.setSpeed(cmd->speed, cmd->forward);
 		car.Control.Direction.setAngle(cmd->angle);
 		break;
 	}
 	default:
+		cout << "(type: " << message.header.type << ")" << endl;
 		break;
 	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,11 @@ int main() {
 	 * Keep the program running for 60 seconds so that the PWM output can be
 	 * observed using an external instrument.
 	 */
-
-	float a = 45;
 	Car.start();
 
 	RemoteControl rc(Car);
 	rc.startServer(1337);
 
-	cout << "Power off then power on the speed regulator" << endl;
 	time_t currentTime;
 	time_t finalTime;
 	time_t startTime;
@@ -32,22 +29,17 @@ int main() {
 	time(&currentTime);
 	finalTime = currentTime + 30;
 	time_t nextInvert = currentTime + 1;
-	using namespace std;
 
 	while (currentTime < finalTime) {
 		if (currentTime >= nextInvert) {
-			std::cout << "current time: " << currentTime - startTime << "\n";
-			//cout << "a: " << a << endl;
-			//Car.Control.Speed.setSpeed(a+=0.1);
-			Car.Control.Direction.setAngle(a = -a);
-
-
-			nextInvert = currentTime + 1;
+			//cout << "Main loop running" << endl;
+			nextInvert = currentTime + 10;
 		}
 		time(&currentTime);
 	}
 
 	rc.stop();
+
 	cout << "end " << endl;
 	} catch(const char* e){
 		cout << e << endl;
